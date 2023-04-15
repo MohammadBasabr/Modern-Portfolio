5eaf7ceddb14d7def47964e7165260975a5b843c
Mohammad Baasabr <m.basabr75@gmail.com>
2023-04-16 01:38:04 +0330

    feat: about section

--- components/About.tsx
+++ components/About.tsx
@@ -5,13 +5,18 @@ type Props = {};
 
 function About({}: Props) {
   return (
-    <div
+    <motion.div
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
       className="flex flex-col relative h-screen text-center
     md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto
     items-center"
     >
       <h3
-        className="absolute top-24 uppercase tracking-[20px] text-gray-500
+        className="sm:absolute top-24 mt-5 md:mt-0 uppercase tracking-[20px] text-gray-500
           text-2xl"
       >
         About
@@ -24,10 +29,23 @@ function About({}: Props) {
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         src="./Mohammad.jpg"
-        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56
-              rounded-full object-cover"
-      ></motion.img>
-    </div>
+        className="md:mb-0 flex-shrink-0 w-44 h-44
+              rounded-full object-cover md:rounded-lg md:w-64 md:h-64 xl:w-[500px] xl:h-[500px]"
+      />
+      <div className="space-y-5 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          here is a{" "}
+          <span className="underline decoration-[#f7ab0a]">little</span>{" "}
+          background
+        </h4>
+        <p className="text-base">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit, nulla
+          vitae. Adipisci amet dolor, facere atque modi ab magni illo sint
+          fugiat laboriosam assumenda, consequatur cum, quisquam molestiae iusto
+          necessitatibus!
+        </p>
+      </div>
+    </motion.div>
   );
 }
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ type Props = {};
 function Header({}: Props) {
   return (
     <header
-      className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20
+      className="sticky top-0 p-2 flex items-start justify-between max-w-7xl mx-auto z-20
       xl:items-center"
     >
       <motion.div
