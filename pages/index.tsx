422f4f602f7aba736ee63859c2845ba20dc4972f
Mohammad Baasabr <m.basabr75@gmail.com>
2023-04-16 01:42:30 +0330

    fix: literal problem

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
-        <title>Mohammad's Portfolio</title>
+        <title>Modern Portfolio</title>
       </Head>
 
       {/* header */}
